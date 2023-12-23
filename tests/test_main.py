794b879679df3b127d8d1b357ad1655e29f6d29f
kot9pa <kot9paa@gmail.com>
2023-12-23 21:30:12 +0200

    Added pytest

---
+++ tests/test_main.py
@@ -0,0 +1,6 @@
+from tickers_history.main import TickersHistory
+
+def test_normalize():
+    th = TickersHistory('tickers.txt', '01.01.20')
+    th.calc_accuracy = None
+    assert th.normalize(100, 100) == 100
